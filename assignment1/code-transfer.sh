50fc4325dac07c409f08137947e7030397a1f793
gg0h <gg0h.ctf@gmail.com>
2021-10-14 18:48:08 +0100

    file restructuring

--- assignment1/code-transfer.sh
+++ assignment1/code-transfer.sh
@@ -10,6 +10,6 @@ else
     echo "no existing tar files cleanup not performed";
 fi
 
-tar -czvf assignment1.tar.gz Assignment1a-dir/ 
+tar -czvf assignment1.tar.gz assignment-1* 
 
 scp -i ~/csc4005/kelvin_id_rsa -P 55890 assignment1.tar.gz 40173582@login.kelvin.alces.network:~/csc4005/assignments/
